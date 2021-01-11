2b29ebe35b869b30d2373ea5d27375540a2e2846
Chris Meazza <christianmeazza@gmail.com>
2021-01-10 22:33:47 -0500

    Initial Commit

--- app/src/main/java/com/meazza/receta/ui/theme/Theme.kt
+++ app/src/main/java/com/meazza/receta/ui/theme/Theme.kt
@@ -7,12 +7,6 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = purple200,
-    primaryVariant = purple700,
-    secondary = teal200
-)
-
 private val LightColorPalette = lightColors(
     primary = purple500,
     primaryVariant = purple700,
@@ -23,6 +17,12 @@ private val LightColorPalette = lightColors(
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
+)
+
+private val DarkColorPalette = darkColors(
+    primary = purple200,
+    primaryVariant = purple700,
+    secondary = teal200
 )
 
 @Composable
